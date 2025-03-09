b2f9425213e33c3e3346cc559a81a19ebacf5316
MartinVR009 <martinvasquezr@outlook.com>
2025-03-09 17:34:44 -0500

    new book

--- ICM_Proyecto01/app/build.gradle.kts
+++ ICM_Proyecto01/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("kotlin-kapt") // Add this line
 }
 
 android {
@@ -53,4 +54,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.glide)
+    kapt(libs.glideCompiler)
+
 }
